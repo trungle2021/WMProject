df8909098973c240c1052a2fd06aa753cf6cd359
khangnqts2104024 <87858814+khangnqts2104024@users.noreply.github.com>
2023-02-24 08:44:32 +0700

    k

--- src/main/java/com/springboot/wmproject/DTO/ServiceDetailDTO.java
+++ src/main/java/com/springboot/wmproject/DTO/ServiceDetailDTO.java
@@ -4,7 +4,8 @@ import com.springboot.wmproject.entities.Orders;
 import com.springboot.wmproject.entities.Services;
 import lombok.Data;
 
-import javax.persistence.*;
+
+
 
 @Data
 public class ServiceDetailDTO {
