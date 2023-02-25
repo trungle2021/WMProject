10542565cade679554fc7cfa5360dadafc01f5f0
khoa954 <87458205+khoa954@users.noreply.github.com>
2023-02-25 18:04:26 +0700

    valid venues + organize team

--- src/main/java/com/springboot/wmproject/DTO/VenueDTO.java
+++ src/main/java/com/springboot/wmproject/DTO/VenueDTO.java
@@ -1,15 +1,23 @@
 package com.springboot.wmproject.DTO;
 
 import com.springboot.wmproject.entities.Orders;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 
 public class VenueDTO {
+
     private int id;
+    @NotEmpty
     private String venueName;
+    @Min(1)
+    @Max(3000)
     private Integer minPeople;
+    @Min(1)
+    @Max(3000)
     private Integer maxPeople;
+    @Min(1)
     private Double price;
 //    private Set<OrderDTO> orderDTOS;
 }
