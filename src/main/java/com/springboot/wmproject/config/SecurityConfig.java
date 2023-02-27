35211f64f8170eda994dab60b42c917b7712e5d8
khangnqts2104024 <87858814+khangnqts2104024@users.noreply.github.com>
2023-02-28 02:34:47 +0700

    teamplate admin

--- src/main/java/com/springboot/wmproject/config/SecurityConfig.java
+++ src/main/java/com/springboot/wmproject/config/SecurityConfig.java
@@ -82,7 +82,8 @@ public class SecurityConfig {
             "/webjars/**",
             // -- Swagger UI v3 (OpenAPI)
             "/v3/api-docs/**",
-            "/swagger-ui/**"
+            "/swagger-ui/**",
+            "/**"
             // other public endpoints of your API may be appended to this array
     };
 
