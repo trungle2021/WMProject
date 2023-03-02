983dc0f27fd151a82ee7be03418c0a324eb4b524
le anh trung <leanhtrung97@gmail.com>
2023-03-02 23:16:04 +0700

    ok

--- src/main/java/com/springboot/wmproject/WebClient/Customer/HomeController.java
+++ src/main/java/com/springboot/wmproject/WebClient/Customer/HomeController.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:src/main/java/com/springboot/wmproject/WebClient/Customer/HomeController.java
 package com.springboot.wmproject.WebClient.Customer;
-========
-package com.springboot.wmproject.WebClient.Servlet.Staff;
->>>>>>>> develop:src/main/java/com/springboot/wmproject/WebClient/Servlet/Staff/HomeController.java
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-<<<<<<<< HEAD:src/main/java/com/springboot/wmproject/WebClient/Customer/HomeController.java
     @GetMapping(value = {"/index","/"})
     public String index(){
         return "home";
@@ -22,23 +17,8 @@ public class HomeController {
     }
 
     @GetMapping("/about")
-    public String about(){
-        return "about";
-========
-    @GetMapping(value = {"/index", "/"})
-    public String index() {
-        return "adminTemplate/index";
-    }
-
-    @GetMapping("/about")
     public String about() {
-        return "adminTemplate/pages/calendar";
->>>>>>>> develop:src/main/java/com/springboot/wmproject/WebClient/Servlet/Staff/HomeController.java
-    }
+        return "about";
 
-    @GetMapping("/venue")
-    public String venue() {
-        return "adminTemplate/pages/gallery";
     }
-
 }
