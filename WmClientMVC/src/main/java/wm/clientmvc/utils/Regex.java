e7a655146f31c34543bc78c5b66d42f5d9a8c557
trungle2021 <leanhtrung97@gmail.com>
2023-03-22 08:51:30 +0700

    update regex phone, email, name

---
+++ WmClientMVC/src/main/java/wm/clientmvc/utils/Regex.java
@@ -0,0 +1,8 @@
+package wm.clientmvc.utils;
+
+public class Regex {
+    public static final String name_vietnamese = "^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\\s|_]+$";
+    public static final String phone_vietnamese = "^(0|(84)|(\\+84)?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$";
+
+    public static final String email = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}\\s*$";
+}
